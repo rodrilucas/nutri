abb88532e4951e8dddfad86bd55f51ef016da67e
rodrilucas <lucas.celery@gmail.com>
2025-05-17 16:54:18 -0300

    feat: set the pagination to one when the search term is changed

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -5,13 +5,19 @@ import useFoodsContext from "../hooks/useFoodsContext";
 import useFatSecret from "../hooks/api/useFatSecret";
 
 function Search() {
-  const { setFoodsResults, setPaginationTerm } = useFoodsContext();
+  const { setFoodsResults, setPaginationTerm, setCurrentPage, paginationTerm } =
+    useFoodsContext();
   const { searchFood } = useFatSecret();
   const [term, setTerm] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const { foods } = await searchFood(term);
+
+    if (paginationTerm !== term) {
+      setCurrentPage(1);
+    }
+
     const parsed: FoodsResults = {
       data: {
         food: foods.food,
@@ -20,6 +26,7 @@ function Search() {
         total_results: Number(foods.total_results),
       },
     };
+    
     setFoodsResults(parsed);
     setPaginationTerm(term);
     setTerm("");
@@ -33,7 +40,7 @@ function Search() {
           name="term"
           value={term}
           onChange={(e) => setTerm(e.target.value)}
-          placeholder="Pesquisar Alimento"
+          placeholder="Pesquisar alimento"
           className="w-full pl-14 bg-[#f2f2f2] dark:text-[#949cae] dark:bg-[#1e2939] py-3 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <Icon className="absolute left-6 top-4 before:w-[16px] before:h-[16px] before:bg-[url(/search.svg)]" />
