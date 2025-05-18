617d8155031c29e8f0d1adb30054950f28aab049
rodrilucas <lucas.celery@gmail.com>
2025-05-18 12:35:15 -0300

    feat: svg icons for simple pagination

---
+++ src/icons/NextIcon.tsx
@@ -0,0 +1,18 @@
+const NextIcon = () => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    viewBox="0 0 14 10"
+    className="w-3.5 h-3.5"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M1 5h12m0 0L9 1m4 4L9 9"
+    />
+  </svg>
+);
+
+export default NextIcon;
---
+++ src/icons/PrevIcon.tsx
@@ -0,0 +1,18 @@
+const PrevIcon = () => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    viewBox="0 0 14 10"
+    className="w-3.5 h-3.5"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M13 5H1m0 0 4 4M1 5l4-4"
+    />
+  </svg>
+);
+
+export default PrevIcon;
